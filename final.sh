3898f3fe9ace881797f07e11c1e7bc3ecb79885f
Diogo Aluai <diogo.aluai@mgm-tp.com>
2025-01-31 20:39:16 +0000

    Add hello endpoint code generation

--- final.sh
+++ final.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-cp hello-endpoint.cbf hello-endpoint.c
+awk '/CBF-ENDPOINT-SWITCH-CASE/ {while ((getline line < "templates/endpoint_switch_case") > 0) print line} 1' hello-endpoint.cbf > hello-endpoint.c
+
+
 gcc hello-endpoint.c -o endpoint-hello
